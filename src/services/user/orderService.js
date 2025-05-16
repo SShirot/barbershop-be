ac180080643d36c6626e0d95fe7354b262fbaf86
Nguyễn Khắc Huy <97592983+SShirot@users.noreply.github.com>
2025-05-16 23:20:50 +0700

    last

--- src/services/user/orderService.js
+++ src/services/user/orderService.js
@@ -1,15 +1,15 @@
-const Repository = require('./../../repositories/OrderRepository');
+const Repository = require("./../../repositories/orderRepository");
 
 const OrderService = {
-    updateStatus: async (id, updateData) => {
-        const isUpdated = await Repository.updateStatus(id, updateData);
-        // return isUpdated ? await Repository.findById(id) : null;
-        return null;
-    },
-    updateStatusPayment: async (id, updateData) => {
-        const isUpdated = await Repository.updateStatusPayment(id, updateData);
-        return isUpdated ? await Repository.findById(id) : null;
-    }
+  updateStatus: async (id, updateData) => {
+    const isUpdated = await Repository.updateStatus(id, updateData);
+    // return isUpdated ? await Repository.findById(id) : null;
+    return null;
+  },
+  updateStatusPayment: async (id, updateData) => {
+    const isUpdated = await Repository.updateStatusPayment(id, updateData);
+    return isUpdated ? await Repository.findById(id) : null;
+  },
 };
 
 module.exports = OrderService;
